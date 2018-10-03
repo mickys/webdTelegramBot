af1b68b52834863859d1ab24e2cde36bb8c42f55
Micky Socaci <micky@nowlive.ro>
2018-10-04 01:49:29 +0300

    Updated data schemas

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var http = require('http');
 
 const BACM_ADDRESS = 'WEBD$gCsh0nNrsZv9VYQfe5Jn$9YMnD4hdyx62n$';
 const WEBD_NETWORK_BLOCKS_URL = 'https://webdollar.network:5001/block';
-const WEBD_NETWORK_PAYMENTS_URL = 'https://webdollar.network:5001/address/'+BACM_ADDRESS+'?show_all_transactions=false';
+const WEBD_NETWORK_PAYMENTS_URL = 'https://webdollar.network:5001/address/' + BACM_ADDRESS + '?show_all_transactions=false';
 
-const TOKEN = 'INSERT_TOKEN_HERE'; 
-const TELEGRAM_URL = 'https://api.telegram.org/'+TOKEN+'/sendMessage';
+const TOKEN = 'INSERT_TOKEN_HERE';
+const TELEGRAM_URL = 'https://api.telegram.org/' + TOKEN + '/sendMessage';
 const CHAT_ID = '@BACMpool';
 
 http.createServer(function (req, res) {
@@ -15,64 +15,68 @@ http.createServer(function (req, res) {
     res.end(); //end the response
 }).listen(8080);
 
-tokenCheck();
-
-if(isTokenSet()){
-    setInterval(function(){
+if (isTokenSet()) {
+    setInterval(function () {
         axios.get(WEBD_NETWORK_BLOCKS_URL)
             .then(function (response) {
-                if(response.status === 200){
+                if (response.status === 200) {
                     console.log('Checking last blocks...');
                     let lastBlock = response.data[0];
+
+                    isValidBlockSchema(lastBlock);
+
                     let lastBlockMined = getLastBlockMinedFromDb();
 
-                    if(lastBlock.miner_address === BACM_ADDRESS && lastBlockMined.block_id !== lastBlock.block_id){
-                        console.log('New block mined!');
+                    if (lastBlock.miner === BACM_ADDRESS && lastBlockMined.number !== lastBlock.number) {
                         writeLastBlockToDb(lastBlock);
+
+                        const message = 'New block mined by BACMpool! #' + lastBlockMined.number
+                        console.log(message);
 
                         axios.get(TELEGRAM_URL, {
                             params: {
                                 chat_id: CHAT_ID,
-                                text: 'New block mined by BACMpool! #'+lastBlockMined.block_id
+                                text: message
                             }
-                        })
-                        .then(function (response) {
+                        }).then(function (response) {
                             console.log("Telegram message delivered successfuly!");
-                        })
-                        .catch(function (error) {
+
+                        }).catch(function (error) {
                             console.log(error);
                         });
                     }
-                
+
                 }
             })
             .catch(function (error) {
                 console.log(error);
-            }); 
+            });
     }, 15000);
 
-    setInterval(function(){
+    setInterval(function () {
         axios.get(WEBD_NETWORK_PAYMENTS_URL)
             .then(function (response) {
-                if(response.status === 200){
-                    if(response.data.transactions.length){
+                if (response.status === 200) {
+                    console.log('Checking last payments...');
+
+                    if (response.data.transactions.length) {
                         let lastPayment = response.data.transactions[0];
                         let lastPaymentFromDb = getLastPaymentFromDb();
 
-                        if(lastPayment.blockId !== lastPaymentFromDb.blockId){
-                            console.log('New payment made!');
+                        if (lastPayment.block_number !== lastPaymentFromDb.block_number) {
                             writeLastPaymentToDb(lastPayment);
+                            const message = 'New payment made by BACMpool! Value: ' + toNormalAmount( lastPayment.from_amount ) + ' WEBD.';
+                            console.log(message);
 
                             axios.get(TELEGRAM_URL, {
                                 params: {
                                     chat_id: CHAT_ID,
-                                    text: 'New payment made by BACMpool! Value: '+lastPayment.from.amount+' WEBD.'
+                                    text: message
                                 }
-                            })
-                            .then(function (response) {
+                            }).then(function (response) {
                                 console.log("Telegram message delivered successfuly!");
-                            })
-                            .catch(function (error) {
+
+                            }).catch(function (error) {
                                 console.log(error);
                             });
 
@@ -83,31 +87,34 @@ if(isTokenSet()){
             })
             .catch(function (error) {
                 console.log(error);
-            }); 
+            });
     }, 60000);
 };
 
+function toNormalAmount( value ) {
+    return value / 10000;
+}
 
-function getLastBlockMinedFromDb(){
+function getLastBlockMinedFromDb() {
     let DB = JSON.parse(fs.readFileSync('db.json', 'utf8'));
     return DB.blocksMined.slice(-1).pop();
 }
 
-function getLastPaymentFromDb(){
+function getLastPaymentFromDb() {
     let DB = JSON.parse(fs.readFileSync('db.json', 'utf8'));
     return DB.paymentsSent.slice(-1).pop();
 }
 
-function writeLastBlockToDb(lastBlock){
-    fs.readFile('db.json', 'utf8', function readDbFile(err, data){
-        if (err){
+function writeLastBlockToDb(lastBlock) {
+    fs.readFile('db.json', 'utf8', function readDbFile(err, data) {
+        if (err) {
             console.log(err);
         }
         else {
-            DB = JSON.parse(data); 
+            DB = JSON.parse(data);
             DB.blocksMined.push({
-                block_id: lastBlock.block_id, 
-                miner_address: lastBlock.miner_address, 
+                number: lastBlock.number,
+                miner: lastBlock.miner,
                 nonce: lastBlock.nonce,
                 timestamp: lastBlock.timestamp
             });
@@ -118,16 +125,16 @@ function writeLastBlockToDb(lastBlock){
     });
 }
 
-function writeLastPaymentToDb(lastPayment){
-    fs.readFile('db.json', 'utf8', function readDbFile(err, data){
-        if (err){
+function writeLastPaymentToDb(lastPayment) {
+    fs.readFile('db.json', 'utf8', function readDbFile(err, data) {
+        if (err) {
             console.log(err);
         }
         else {
-            DB = JSON.parse(data); 
+            DB = JSON.parse(data);
             DB.paymentsSent.push({
-                blockId: lastPayment.blockId, 
-                value: lastPayment.from.amount, 
+                block_number: lastPayment.block_number,
+                from_amount: lastPayment.from_amount,
                 timestamp: lastPayment.timestamp
             });
             json = JSON.stringify(DB);
@@ -135,17 +142,25 @@ function writeLastPaymentToDb(lastPayment){
             console.log('DB lastPayment write successful!');
         }
     });
+
 }
 
-function tokenCheck(){
-    if(TOKEN === 'INSERT_TOKEN_HERE'){
-        console.log('Please insert the telegram bot token by replacing the INSERT_TOKEN_HERE wih your token.');
-        return true;
+function isValidBlockSchema(data) {
+
+    // we have a block number
+    if (data.number === undefined) {
+        throw new Error("API did not return a proper block number");
     }
+
+    if (data.miner === undefined) {
+        throw new Error("API did not return a proper miner address");
+    }
+    return true;
 }
 
-function isTokenSet(){
-    if(TOKEN === 'INSERT_TOKEN_HERE'){
+function isTokenSet() {
+    if (TOKEN === 'INSERT_TOKEN_HERE') {
+        console.log('Please insert the telegram bot token by replacing the INSERT_TOKEN_HERE wih your token.');
         return false;
     }
 
